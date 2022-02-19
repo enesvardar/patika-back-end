89b9ded1a3cd241134fe0780689d66e30c272826
enes vardar <enesvrdr0@gmail.com>
2022-02-19 20:58:36 +0300

    add express

--- node-js/CleanBlog/app.js
+++ node-js/CleanBlog/app.js
@@ -1,15 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
+const app = express();
 
-app.get('/', (req, res) => {
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
+//MIDDLEWARES
 
-    res.send(blog)
-})
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.static('public'));
+app.use(myLogger);
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.get('/add_post', function (req, res) {
+  res.render('add_post');
+});
+
+app.get('/post', function (req, res) {
+    res.render('post');
+  });
+
+const port = 3000;
+
+app.listen(3000, () => {
+  console.log('Sunucu %d portunda başlatıldı...', port);
+});
