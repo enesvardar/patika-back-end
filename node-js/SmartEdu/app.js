baed86d15a37b2db24229c5c6d6a25744cab263a
enes vardar <enesvrdr0@gmail.com>
2022-02-26 23:03:21 +0300

    MVC eklendi

--- node-js/SmartEdu/app.js
+++ node-js/SmartEdu/app.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (res, req) => {
-  res.send('index sayfası');
-});
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/home', (req, res) => {
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/courses', (req, res) => {
+  res.render('courses')
+})
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
 
 const port = 3000;
 
